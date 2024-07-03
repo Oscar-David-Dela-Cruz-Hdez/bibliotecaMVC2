ae9341ecb2766978c4167ee51e30928d0f115af9
kevin <kevinlarioshernandez@gmail.com>
2024-07-02 21:33:34 -0600

    commit

--- app/views/inc/testa.php
+++ app/views/inc/testa.php
@@ -1,9 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo APP_NOME; ?></title>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo APP_NOME; ?></title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/sweetalert2.min.css">
-    <script src="<?php echo APP_URL; ?>app/views/js/sweetalert2.all.min.js" ></script>
+<!-- creo que es mejor cargar el sweetalert desde el cdn en linea no?  -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
